255dc5645ee28e3bd3be34d6abca9839f59cd06b
kumudiaorong <kmdr.error@gmail.com>
2024-03-03 13:13:11 +0800

    fix:update dep

--- src/server/main.rs
+++ src/server/main.rs
@@ -40,17 +40,6 @@ impl daemon::main_server::Main for Main {
         &self,
         _: tonic::Request<Empty>,
     ) -> std::result::Result<tonic::Response<daemon::FastConfig>, tonic::Status> {
-        // let mut res = std::collections::HashMap::new();
-        // for (_, v) in self.config.lock().file.exts.iter() {
-        //     if v.addr != "" {
-        //         res.insert(v.prompt.clone(), v.addr.clone());
-        //     }
-        // }
-        // Ok(tonic::Response::new(server::SetUpResult {
-        //     mresult: Some(server::set_up_result::Mresult::Ok(
-        //         server::set_up_result::MOk { running: res },
-        //     )),
-        // }))
         let binding = self.config.lock().unwrap();
         let fexts = binding
             .inner
@@ -73,22 +62,6 @@ impl daemon::main_server::Main for Main {
         log::info(format!("get config: {:#?}", fexts).as_str());
         Ok(tonic::Response::new(daemon::FastConfig { fexts }))
     }
-    // async fn get_config(
-    //     &self,
-    //     _: tonic::Request<Empty>,
-    // ) -> std::result::Result<tonic::Response<daemon::Config>, tonic::Status> {
-    //     let exts = self.config.lock().unwrap().file.exts.clone();
-    //     return Ok(tonic::Response::new(daemon::Config { exts }));
-    // }
-
-    // async fn set_config(
-    //     &self,
-    //     request: tonic::Request<daemon::Config>,
-    // ) -> std::result::Result<tonic::Response<Empty>, tonic::Status> {
-    //     self.config.lock().unwrap().file.exts = request.into_inner().exts;
-    //     return Ok(tonic::Response::new(Empty {}));
-    // }
-
     async fn get_ext_addr(
         &self,
         request: tonic::Request<daemon::ExtId>,
