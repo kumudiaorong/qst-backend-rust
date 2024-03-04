a470175b3326be2054fc98a9ae355c369959fd6c
kumudiaorong <kmdr.error@gmail.com>
2024-03-04 19:54:07 +0800

    fix:use safe shutdown

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,36 @@
 use std::{
+    collections::HashMap,
+    future::Future,
+    pin::Pin,
     sync::{Arc, Mutex},
-    thread::spawn,
 };
 
 use serde::{Deserialize, Serialize};
 use xcfg::File as XFile;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ExtInfo {
+    pub name: String,
+    pub prompt: String,
+    pub dir: String,
+    pub exec: String,
+    pub addr: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ExtRuntimeInfo {
+    pub expire: HashMap<String, i32>,
+}
+impl Default for ExtRuntimeInfo {
+    fn default() -> Self {
+        Self {
+            expire: HashMap::new(),
+        }
+    }
+}
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Info {
-    pub exts: std::collections::HashMap<String, super::server::ExtInfo>,
+    pub exts: std::collections::HashMap<String, ExtInfo>,
 }
 impl Default for Info {
     fn default() -> Self {
@@ -16,24 +39,37 @@ impl Default for Info {
         }
     }
 }
-pub type Config = Arc<Mutex<XFile<Info>>>;
-pub fn init() -> Config {
+pub type Store = Arc<Mutex<XFile<Info>>>;
+pub type Runtime = Arc<Mutex<ExtRuntimeInfo>>;
+#[derive(Clone)]
+pub struct Config {
+    pub store: Store,
+    pub runtime: Runtime,
+}
+
+pub fn init() -> (Config, Pin<Box<dyn Future<Output = ()>>>) {
     let path = dirs::home_dir().unwrap().join(".config/qst/backend.toml");
     let mut file: XFile<Info> = XFile::new().path(path.to_str().unwrap());
     let _ = file.load();
     let _ = file.save();
     let file = Arc::new(Mutex::new(file));
     let move_file = file.clone();
-    spawn(|| {
+    let signal = async move {
         let saver = xcfg::keep::Saver::new(move_file);
         loop {
             match saver.run() {
                 Ok(xcfg::keep::Action::TermSave) => {
-                    std::process::exit(0);
+                    break;
                 }
                 _ => {}
             }
         }
-    });
-    file
+    };
+    (
+        Config {
+            store: file.clone(),
+            runtime: Arc::new(Mutex::new(ExtRuntimeInfo::default())),
+        },
+        Box::pin(signal),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use std::net::{Ipv4Addr, SocketAddr};
 async fn main() -> Result<(), tonic::transport::Error> {
     xlog::log::init(std::io::stdout(), xlog::log::Level::Trace);
     let args = arg::Args::parse();
-    let amc = config::init();
+    let (amc, signal) = config::init();
     tonic::transport::Server::builder()
         .add_service(server::MainServer::new(server::Main::new(amc.clone())))
         .add_service(server::ExtServer::new(server::Ext::new(amc)))
-        .serve(SocketAddr::new(
-            std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            args.port,
-        ))
-        .await
+        .serve_with_shutdown(
+            SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), args.port),
+            signal,
+        )
+        .await?;
+    Ok(())
 }
